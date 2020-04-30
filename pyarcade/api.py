a6eea2d9db35fa9a3b18d91daa8f9be01ec2ac4f
Jillian <jgranad3@umd.edu>
2020-04-30 13:23:48 -0400

    Added game api for saves

--- pyarcade/api.py
+++ pyarcade/api.py
@@ -35,15 +35,6 @@ class User(UserMixin, db.Model):
     passwd = db.Column(db.String(255), unique=False, nullable=False)
 
 
-class GameDB(db.Model):
-    __tablename__ = 'GameDB'
-
-    id = db.Column(db.Integer, primary_key=True)
-    player_id = db.Column(db.Integer, primary_key=True)
-    save_name = db.Column(db.String(128), unique=True, nullable=False)
-    save = db.Column(db.BLOB, unique=False, nullable=False)
-
-
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -128,6 +119,93 @@ class UserResource(Resource):
 api.add_resource(UserListResource, '/users')
 api.add_resource(UserResource, '/users/<int:user_id>')
 
+class Game(db.Model):
+    __tablename__ = 'GameDB'
+
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, primary_key=True)
+    save_name = db.Column(db.String(128), unique=True, nullable=False)
+    save = db.Column(db.BLOB, unique=False, nullable=False)
+
+class GameListResource(Resource):
+    """ A Resource is a collection of routes (think URLs) that map to these functions.
+    For a REST API, we have GET, PUT, POST, PATCH, DELETE, etc. Here we just define
+    functions that map to the REST API verbs, later we map this to a specific URL
+    with api.add_resource
+    """
+
+    def get(self) -> List[dict]:
+        """Responds to http://[domain or IP]:[port (default 5000)]/game
+
+        Returns:
+            List of dictionaries describing all games in the database.
+        """
+        return [{"save_name": game.save_name, "id": game.id} for game in Game.query.all()]
+
+    def post(self) -> dict:
+        """Responds to http://[domain or IP]:[port (default 5000)]/games.
+
+        Adds a new game to the database.
+
+        Returns:
+            Dictionary describing game that was just created.
+        """
+        new_save = Game(player_id=request.json['player_id'], save_name=request.json['save_name'],
+                        save=request.json['save'])
+        db.session.add(new_save)
+        db.session.commit()
+        return {"save_name": request.json["save_name"]}
+
+
+class GameResource(Resource):
+    """ GameResource is slightly different from GameListResource as these functions will only respond
+    to Responds to http://[domain or IP]:[port (default 5000)]/games/<game_id> so these are always
+    executed in the context of a specific game.
+
+    """
+
+    def get(self, game_id):
+        """Responds to http://[domain or IP]:[port (default 5000)]/games/<game_id>
+
+        Returns:
+            Dictionary describing user by game_id
+        """
+        game = Game.query.get_or_404(game_id)
+        return {"id": game.id, "save_name": game.save_name}
+
+    def patch(self, game_id):
+        """Responds to http://[domain or IP]:[port (default 5000)]/games/<game_id>
+
+        This is used to update an existing game.
+
+        Returns:
+           Dictionary describing game that was changed.
+        """
+        game = Game.query.get_or_404(game_id)
+
+        if 'save_name' in request.json:
+            game.save_name = request.json['save_name']
+
+        db.session.commit()
+        return {"id": game.id, "save_name": game.save_name}
+
+    def delete(self, game_id):
+        """Responds to http://[domain or IP]:[port (default 5000)]/games/<game_id>
+
+        This is used to delete an existing game
+
+        Returns:
+           Dictionary describing game that was changed.
+        """
+        game = Game.query.get_or_404(game_id)
+        db.session.delete(game)
+        db.session.commit()
+        return '', 204
+
+
+api.add_resource(GameListResource, '/games')
+api.add_resource(GameResource, '/games/<int:game_id>')
+
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -154,18 +232,18 @@ def mastermind():
     form = GameForm()
 
     user_input = "New Game"
-    if form.validate_on_submit():
-        user_input = form.input.data
-        output_lines = input_system.handle_game_input('mastermind', user_input).splitlines(False)
-        return render_template('mastermind.html', form=form, output_lines=output_lines)
-
     if request.method == "POST":
-        game_option = request.form["option"]
-        if game_option == "Quit":
-            return render_template('dashboard.html', name=current_user.username)
-        else:
-            output_lines = input_system.handle_game_input('mastermind', game_option.lower()).splitlines(False)
+        if form.validate_on_submit():
+            user_input = form.input.data
+            output_lines = input_system.handle_game_input('mastermind', user_input).splitlines(False)
             return render_template('mastermind.html', form=form, output_lines=output_lines)
+        else:
+            game_option = request.form["option"]
+            if game_option == "Quit":
+                return redirect(url_for('dashboard'))
+            else:
+                output_lines = input_system.handle_game_input('mastermind', game_option.lower()).splitlines(False)
+                return render_template('mastermind.html', form=form, output_lines=output_lines)
 
     output_lines = input_system.handle_game_input('mastermind', user_input).splitlines(False)
     return render_template('mastermind.html', form=form, output_lines=output_lines)
